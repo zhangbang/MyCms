3178cdabc876343782075b767202e92dcd65cf75
张邦 <386654667@qq.com>
2021-06-30 00:52:12 +0800

    create user & system & module

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
     ],
 
     /*
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Module' => Nwidart\Modules\Facades\Module::class,
     ],
 
 ];
