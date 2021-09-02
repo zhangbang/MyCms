6910afa434c115e4838fce2c818cc1e8b7535a80
张邦 <386654667@qq.com>
2021-09-02 19:53:41 +0800

    v1.1.3.1 新增百度统计

--- Modules/Cms/Resources/views/web/default/layouts/_header.blade.php
+++ Modules/Cms/Resources/views/web/default/layouts/_header.blade.php
@@ -33,6 +33,16 @@
     <script src="/mycms/cms/theme/default/assets/js/respond.js"></script>
     <![endif]-->
     <!-- ======== / STANDARD SCRIPTS ======== -->
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?96c4c531b8527a0c49252e95fb78bd9d";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+
 </head>
 <body>
 
