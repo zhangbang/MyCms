6ae7b0560a29db75c263d00eff19ef4fcbbc88b1
zhangbang <386654667@qq.com>
2021-12-10 09:41:59 +0800

    v2.0
    [更新内容]
    
    新增：API-省市县地区接口
    新增：地区数据表(含数据)
    新增：插件显示到菜单功能
    新增：客户端类型判断
    新增：客户端浏览器类型判断
    优化：插件读取功能
    优化：获取UserAgent函数
    优化：绑定SystemService使用
    修正：参数安全过滤默认值

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\System\Database\Seeders\RegionSeeder;
 use Modules\System\Database\Seeders\SystemAdminSeeder;
 use Modules\System\Database\Seeders\SystemConfigSeeder;
 use Modules\System\Database\Seeders\SystemMenuSeeder;
@@ -21,5 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SystemConfigSeeder::class);
         $this->call(SystemMenuSeeder::class);
         $this->call(SystemRoleSeeder::class);
+        $this->call(RegionSeeder::class);
     }
 }
